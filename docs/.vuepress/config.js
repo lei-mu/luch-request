c7a6d2fe4fbda96c5a5343718852088c093c813a
luch <webwork.s@qq.com>
2020-07-13 00:34:08 +0800

    docs: 更换360 收录代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,11 +25,8 @@ module.exports = {
         `],
     ['script', {}, `
 ;(function(){
- var hm = document.createElement("script");
-  hm.src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-  hm.id = "sozz"
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
+var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+document.write('<script src="' + src + '" id="sozz"><\\/script>');
 })();
         `]
   ],
