d7ee8e5cabfbb4caa45558bd03a5b1a70adc73aa
Mr_Mao <wwu710632@gmail.com>
2020-09-05 17:20:38 +0800

    Mr_Mao 2020-9-5 17:20:25 同步index.d.ts说明文件

--- DCloud/luch-request/index.d.ts
+++ DCloud/luch-request/index.d.ts
@@ -1,9 +1,15 @@
-export type LooseObject = Record<string | number | symbol, any>
-export type DiffKeys<K extends string | number | symbol> = keyof Record<K, never>
-export type LuchPromise<T = any> = Promise<LuchResponse<T>>
+export type LooseObject = Record<string | number | symbol, any>;
+export type DiffKeys<K extends string | number | symbol> = keyof Record<K, never>;
+export type LuchPromise<T = any> = Promise<LuchResponse<T>>;
+
+export interface RequestTask {
+  abort: () => void;
+  offHeadersReceived: () => void;
+  onHeadersReceived: () => void;
+};
 
 export interface LuchRequestConfig {
-  baseUrl?: string;
+  baseURL?: string;
   url?: string;
 
   params?: LooseObject;
@@ -21,41 +27,64 @@ export interface LuchRequestConfig {
   sslVerify?: boolean;
   withCredentials?: boolean;
 
-  getTask?: (task: any, options: any) => void;
+  getTask?: (task: RequestTask, options: LuchRequestConfig) => void;
   validateStatus?: (statusCode: number) => boolean | void;
-}
+};
 
 export interface LuchResponse<T = any> {
-  statusCode: number;
   config: LuchRequestConfig;
+  statusCode: number;
+  cookies: Array<string>;
   data: T;
+  errMsg: string;
+  header: LooseObject;
+};
+
+export interface LuchDownloadResponse extends LuchResponse {
+  tempFilePath: string;
 }
+
+export interface LuchError {
+  config: LuchRequestConfig;
+  statusCode?: number;
+  cookies?: Array<string>;
+  data?: any;
+  errMsg: string;
+  header?: LooseObject;
+};
 
 export abstract class LuchRequestAbstract {
   constructor(config?: LuchRequestConfig);
   config: LuchRequestConfig;
   interceptors: {
     request: {
-      use(onSend?: (config: LuchRequestConfig) => any): LuchRequestConfig | void;
+      use(
+        onSend?: (config: LuchRequestConfig) => LuchRequestConfig
+      ): void;
     };
     response: {
-      use(onSend?: (response: LuchResponse) => any, onError?: (response: LuchResponse) => any): void;
+      use(
+        onSend?: (response: LuchResponse) => LuchResponse,
+        onError?: (response: LuchError) => LuchError | Promise<LuchError>
+      ): void;
     };
   };
-  middleware<T>(config: LuchRequestConfig): LuchPromise<T>
-  request<T>(config: LuchRequestConfig): LuchPromise<T>
-  get<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  upload<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  delete<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  head<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  post<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  put<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  connect<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  options<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  trace<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
+  middleware<T>(config: LuchRequestConfig): LuchPromise<T>;
+  request<T>(config: LuchRequestConfig): LuchPromise<T>;
+  get<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  upload<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  delete<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  head<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  post<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  put<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  connect<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  options<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  trace<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
 
+  download(url: string, config?: LuchRequestConfig): Promise<LuchDownloadResponse>;
+  
   setConfig(onSend: (config: LuchRequestConfig) => LuchRequestConfig | void): void;
-}
+};
 
-declare class LuchRequest extends LuchRequestAbstract {}
-export default LuchRequest
+declare class LuchRequest extends LuchRequestAbstract { };
+export default LuchRequest;
