e97cde40ca6c25431f0c5842870940f447dc5b79
Mr_Mao <wwu710632@gmail.com>
2020-09-05 16:35:51 +0800

    Mr_Mao 2020-9-5 16:35:09 添加task说明文件，补充请求失败静态类型。

--- src/lib/luch-request.d.ts
+++ src/lib/luch-request.d.ts
@@ -1,6 +1,12 @@
-export type LooseObject = Record<string | number | symbol, any>
-export type DiffKeys<K extends string | number | symbol> = keyof Record<K, never>
-export type LuchPromise<T = any> = Promise<LuchResponse<T>>
+export type LooseObject = Record<string | number | symbol, any>;
+export type DiffKeys<K extends string | number | symbol> = keyof Record<K, never>;
+export type LuchPromise<T = any> = Promise<LuchResponse<T>>;
+
+export interface RequestTask {
+  abort: () => void;
+  offHeadersReceived: () => void;
+  onHeadersReceived: () => void;
+};
 
 export interface LuchRequestConfig {
   baseURL?: string;
@@ -20,17 +26,28 @@ export interface LuchRequestConfig {
   timeout?: number;
   sslVerify?: boolean;
   withCredentials?: boolean;
-  firstIpv4?: boolean;
 
-  getTask?: (task: any, options: any) => void;
+  getTask?: (task: RequestTask, options: LuchRequestConfig) => void;
   validateStatus?: (statusCode: number) => boolean | void;
-}
+};
 
 export interface LuchResponse<T = any> {
-  statusCode: number;
   config: LuchRequestConfig;
+  statusCode: number;
+  cookies: Array<string>;
   data: T;
-}
+  errMsg: string;
+  header: LooseObject;
+};
+
+export interface LuchError {
+  config: LuchRequestConfig;
+  statusCode?: number;
+  cookies?: Array<string>;
+  data?: any;
+  errMsg: string;
+  header?: LooseObject;
+};
 
 export abstract class LuchRequestAbstract {
   constructor(config?: LuchRequestConfig);
@@ -40,23 +57,23 @@ export abstract class LuchRequestAbstract {
       use(onSend?: (config: LuchRequestConfig) => any): LuchRequestConfig | void;
     };
     response: {
-      use(onSend?: (response: LuchResponse) => any, onError?: (response: LuchResponse) => any): void;
+      use(onSend?: (response: LuchResponse) => any, onError?: (response: LuchError) => any): void;
     };
   };
-  middleware<T>(config: LuchRequestConfig): LuchPromise<T>
-  request<T>(config: LuchRequestConfig): LuchPromise<T>
-  get<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  upload<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  delete<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  head<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  post<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  put<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  connect<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  options<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
-  trace<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>
+  middleware<T>(config: LuchRequestConfig): LuchPromise<T>;
+  request<T>(config: LuchRequestConfig): LuchPromise<T>;
+  get<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  upload<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  delete<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  head<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  post<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  put<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  connect<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  options<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
+  trace<T>(url: string, config?: LuchRequestConfig): LuchPromise<T>;
 
   setConfig(onSend: (config: LuchRequestConfig) => LuchRequestConfig | void): void;
-}
+};
 
-declare class LuchRequest extends LuchRequestAbstract {}
-export default LuchRequest
+declare class LuchRequest extends LuchRequestAbstract { };
+export default LuchRequest;
