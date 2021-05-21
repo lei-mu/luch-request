0a20cd1641498e347dd2a9e74a7fbcf944471571
luch <webwork.s@qq.com>
2021-05-21 19:27:42 +0800

    docs: 删除360收录js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,12 +22,6 @@ module.exports = {
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-        `],
-    ['script', {}, `
-;(function(){
-var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-document.write('<script src="' + src + '" id="sozz"><\\/script>');
-})();
         `]
   ],
   themeConfig: {
