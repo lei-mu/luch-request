ea84ba76f9aaa056e0435c63cc0534ae63faabbb
luch <webwork.s@qq.com>
2021-09-04 15:59:43 +0800

    refactor: 修改版本号

--- src/lib/core/Request.js
+++ src/lib/core/Request.js
@@ -1,9 +1,9 @@
 /**
  * @Class Request
  * @description luch-request http请求插件
- * @version 3.0.6
+ * @version 3.0.7
  * @Author lu-ch
- * @Date 2021-05-10
+ * @Date 2021-09-04
  * @Email webwork.s@qq.com
  * 文档: https://www.quanzhan.co/luch-request/
  * github: https://github.com/lei-mu/luch-request
