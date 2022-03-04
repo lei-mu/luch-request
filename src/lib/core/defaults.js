8b6b4ad9351fe8cf7b4cb892c9c9629d99a229e3
luch <webwork.s@qq.com>
2022-03-04 11:10:34 +0800

    重构逻辑

--- src/lib/core/defaults.js
+++ src/lib/core/defaults.js
@@ -8,7 +8,7 @@ export default {
   header: {},
   method: 'GET',
   dataType: 'json',
-  paramsSerializer:null,
+  paramsSerializer: null,
   // #ifndef MP-ALIPAY
   responseType: 'text',
   // #endif
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -105,31 +105,40 @@ export function isPlainObject(obj) {
 
 
 
+
 /**
- * Function equal to merge with the difference being that no reference
- * to original objects is kept.
+ * Accepts varargs expecting each argument to be an object, then
+ * immutably merges the properties of each object and returns result.
  *
- * @see merge
+ * When multiple objects contain the same key the later object in
+ * the arguments list will take precedence.
+ *
+ * Example:
+ *
+ * ```js
+ * var result = merge({foo: 123}, {foo: 456});
+ * console.log(result.foo); // outputs 456
+ * ```
+ *
  * @param {Object} obj1 Object to merge
  * @returns {Object} Result of all merge properties
  */
-export function deepMerge(/* obj1, obj2, obj3, ... */) {
-  let result = {}
+export function merge(/* obj1, obj2, obj3, ... */) {
+  var result = {}
   function assignValue(val, key) {
-    if (typeof result[key] === 'object' && typeof val === 'object') {
-      result[key] = deepMerge(result[key], val)
-    } else if (typeof val === 'object') {
-      result[key] = deepMerge({}, val)
+    if (isPlainObject(result[key]) && isPlainObject(val)) {
+      result[key] = merge(result[key], val)
+    } else if (isPlainObject(val)) {
+      result[key] = merge({}, val)
+    } else if (isArray(val)) {
+      result[key] = val.slice()
     } else {
       result[key] = val
     }
   }
-  for (let i = 0, l = arguments.length; i < l; i++) {
+
+  for (var i = 0, l = arguments.length; i < l; i++) {
     forEach(arguments[i], assignValue)
   }
   return result
-}
-
-export function isUndefined (val) {
-  return typeof val === 'undefined'
 }
