2d6fba8beda1ceddea5234acc04bf1142098854b
luch <webwork.s@qq.com>
2026-08-05 16:33:26 +0800

    ci: 修改脚本

--- node/zipDCloudDemo.js
+++ node/zipDCloudDemo.js
@@ -1,6 +1,9 @@
 var fs = require('fs')
 var archiver = require('archiver')
-
+// 如果 zipDist 文件夹不存在，就创建它
+if (!fs.existsSync('zipDist')) {
+  fs.mkdirSync('zipDist')
+}
 var output = fs.createWriteStream('zipDist/request-demo.zip')
 
 var archive = archiver('zip')
--- node/zipDCloudPlugin.js
+++ node/zipDCloudPlugin.js
@@ -1,6 +1,10 @@
 var fs = require('fs')
 var archiver = require('archiver')
 
+// 如果 zipDist 文件夹不存在，就创建它
+if (!fs.existsSync('zipDist')) {
+  fs.mkdirSync('zipDist')
+}
 var output = fs.createWriteStream('zipDist/luch-request.zip')
 
 var archive = archiver('zip')
